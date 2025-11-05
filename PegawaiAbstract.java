71f48c5c61a40ca5e509dd7b4b92093dbfbb6463
NadineAgietha <agiethanadine@gmail.com>
2025-11-05 15:36:07 +0700

    Fix comments for clarity in PegawaiAbstract.java

--- PegawaiAbstract.java
+++ PegawaiAbstract.java
@@ -3,24 +3,24 @@ public abstract class PegawaiAbstract {
     protected String nip;
     protected double gajiPokok;
 
-    // Constructor non-abstrak
+    // Constructor non-abstract
     public PegawaiAbstract(String nama, String nip, double gajiPokok) {
         this.nama = nama;
         this.nip = nip;
         this.gajiPokok = gajiPokok;
     }
 
-    // Method non-abstrak (Inheritance)
+    // Method non-abstract (Inheritance)
     public void tampilkanDataPegawai() {
         System.out.println("Nama: " + nama);
         System.out.println("NIP: " + nip);
         System.out.println("Gaji Pokok: Rp " + gajiPokok);
     }
     
-    // Method abstrak (Wajib di-implementasi oleh turunan)
+    // Method abstrak diimplementasi di kelas turunan
     public abstract double hitungGajiBersih();
     
-    // Getter
+    // method Getter
     public double getGajiPokok() {
         return gajiPokok;
     }
